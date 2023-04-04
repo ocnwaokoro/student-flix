83a6e642bb8d7655404c25624eb0b3d8705c3a33
ocnwaokoro <chibuikem.nwaokoro@gmail.com>
2023-04-03 22:33:24 -0400

    AWS integration

--- student-flix/pages/api/youtube/video/[id].ts
+++ student-flix/pages/api/youtube/video/[id].ts
@@ -2,7 +2,14 @@ import { NextApiRequest, NextApiResponse } from "next";
 import serverAuth from "@/lib/serverAuth";
 import ytdl from 'ytdl-core';
 import { send } from 'micro';
-
+import AWS from 'aws-sdk'
+const s3 = new AWS.S3(
+  {
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET,
+    region: process.env.AWS_REGION
+  }
+)
 
 export default async function handler(
   req: NextApiRequest,
@@ -11,6 +18,9 @@ export default async function handler(
   if (req.method !== "GET") {
     return res.status(405).end();
   }
+
+  // const data = await s3.listBuckets().promise();
+  // console.log('Bucket Names:', data?.Buckets?.map(bucket => bucket.Name));
 
   const { id } = req.query
   const videoUrl = `https://www.youtube.com/watch?v=${id}`;
@@ -22,11 +32,17 @@ export default async function handler(
   
   res.setHeader('Content-Type', 'video/mp4');
 
-  if (process.env.VERCEL) {
-    // Using Vercel, so use Vercel's `send` method to send the response
-    send(res, 200, videoStream);
-  } else {
-    // Using Next.js, so use Next.js's `send` method to send the response
-    videoStream.pipe(res);
+  const uploadParams = {
+    Bucket: process.env.AWS_BUCKET as string,
+    Key: `${id}.mp4`,
+    Body: videoStream,
+    ContentType: 'video/mp4'
   }
+
+  await s3.upload(uploadParams).promise();
+
+  const redirectUrl = `https://${process.env.AWS_BUCKET}.s3.amazonaws.com/${uploadParams.Key}`
+
+  res.status(300).redirect(redirectUrl)
+
 }
